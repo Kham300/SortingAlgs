0d4eeb1cd3b97a14f47af11505a287db085b32de
Kham Mamytov <boogaga3000@gmail.com>
2018-02-10 12:09:35 +0300

    ResolvingCreative Exercises from Book

--- ElementarySorts/src/SortCompare.java
+++ ElementarySorts/src/SortCompare.java
@@ -3,6 +3,7 @@ public class SortCompare {
         Stopwatch timer = new Stopwatch();
         if (alg.equals("Вставки")) Insertion.sort(a);
         if (alg.equals("Выбор")) Selection.sort(a);
+        if (alg.equals("Шелл")) Shell.sort(a);
         return timer.elapsedTime();
     }
 
@@ -21,11 +22,12 @@ public class SortCompare {
     public static void main(String[] args) {
         String alg1 = "Вставки";
         String alg2 = "Выбор";
-        int N = 1000;
+        String alg3 = "Шелл";
+        int N = 10000;
         int T = 100;
         double t1 = timeRandomInput(alg1, N, T);
-        double t2 = timeRandomInput(alg2, N, T);
-        StdOut.printf("Для %d случайных Doubles\n   %s в", N, alg1);
-        StdOut.printf(" %.1f раз быстрее, чем %s\n", t2/t1, alg2);
+        double t2 = timeRandomInput(alg3, N, T);
+        StdOut.printf("Для %d случайных Doubles\n   %s в", N*T, alg3);
+        StdOut.printf(" %.5f раз быстрее, чем %s\n", t1/t2, alg1);
     }
 }
